edaa96f19a05837eb60f860e5e1d51617a46eff6
kaqui-null <emanuelgomes24.08@gmail.com>
2025-04-07 16:25:11 -0300

    player model and animations

--- scripts/PlayerMovement.gd
+++ scripts/PlayerMovement.gd
@@ -4,6 +4,12 @@ extends CharacterBody3D
 @export var jump_velocity: float
 @export var gravity: float
 
+@onready var animation_tree = $Character/AnimationTree
+@onready var animation_state = $Character/AnimationTree.get("parameters/playback")
+
+var jumping = false
+var last_floor = true
+
 func _physics_process(delta):
 	var movement_vector = get_movement_vector()
 	
@@ -24,16 +30,34 @@ func change_velocity(movement_vector: Vector3, delta):
 	velocity.x = movement_vector.x * speed
 	velocity.z = movement_vector.z * speed
 	
+	# Transicao de animacoes e velocidade
+	animation_tree.set("parameters/Idle-Run_loop/blend_position", Vector2(movement_vector.x, -movement_vector.z))
+	
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
 		if Input.is_action_just_pressed("jump"):
 			velocity.y = jump_velocity
-			
+			jumping = true
+			animation_tree.set("parameters/conditions/jumping", true)
+			animation_tree.set("parameters/conditions/grounded", false)
+	
+	if (is_on_floor() and not last_floor):
+		jumping = false
+		animation_tree.set("parameters/conditions/jumping", false)
+		animation_tree.set("parameters/conditions/grounded", true)
+	
+	if not is_on_floor() and not jumping:
+		animation_state.travel("Jump_Idle")
+		animation_tree.set("parameters/conditions/grounded", false)
+	
+	last_floor = is_on_floor()
 	# girar o player
 	if movement_vector != Vector3.ZERO:
 		var look_direction = Vector3(movement_vector.x, 0, movement_vector.z)
 		look_at(global_transform.origin + look_direction, Vector3.UP) 
+		
+	
 		
 		# TODO: smooth linear interpolation pra deixar a rotacao mais natural
 		
